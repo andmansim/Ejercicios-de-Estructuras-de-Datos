f8d282722bdb4f7f28ad9f1f34efa69aea16979a
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-17 16:17:07 +0200

    view

---
+++ Ejercicio 2/view.py
@@ -0,0 +1,13 @@
+lista = []
+def visitante():
+    
+    print('Introduce una frase')
+    usuario = input()
+    lista.append (usuario)
+    print('Introduce otra frase')
+    usuarioq = input()
+    lista.append (usuarioq)
+    
+    
+visitante()
+print (lista)
