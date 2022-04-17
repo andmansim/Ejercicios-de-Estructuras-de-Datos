520a55585b41a50a6eae3cc79cd784d1774c2d5a
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-17 13:57:31 +0200

    estructura ejercicio 3

---
+++ Ejercicio3.py
@@ -0,0 +1,22 @@
+class Producto:
+    def __init__(self):
+        pass
+    
+
+class Naturaleza:
+    def __init__(self):
+        pass
+    def alimentaria(self):
+        return pass
+    def servicio(self):
+        return pass
+    
+class FactoryFactura:
+    def __init__(self):
+        pass
+    def crear(self):
+        pasa el producto
+    
+    def facturar():
+        pass
+    
