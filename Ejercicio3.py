cfe99dea67f865fdd4229f87634417a602894d9a
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-17 15:02:27 +0200

    modifico el método facturar y cambio la clase Naturaleza a enumerado

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,10 +1,15 @@
 class Producto:
     def __init__(self, tipo):
         self.tipo = tipo
-    def get_tipo(self):
-        return self.tipo
+        self.precio = 100
+        
+    def facturar(self):
+        precio_final = self.precio + (self.precio * self.tipo)
+        return precio_final
 
 class Naturaleza:
+    ALIMENTACION = 5.5/100
+    SERVICIO = 20/100
     def __init__(self):
         self.iva_a = 5.5/100
         self.iva_s = 20/100
@@ -14,19 +19,15 @@ class Naturaleza:
         return self.iva_s
     
 class FactoryFactura:
-    def __init__(self):
-        self.precio = 100
     def crear(self, productos):
         self.producto = productos
         return self.producto
-    def facturar(self):
-        self.precio_final = self.precio + (self.precio * self.producto)
-        return self.precio_final
+    
 
 a = Naturaleza()
 producto = Producto(a.alimentaria)
 print(producto)
 b = FactoryFactura()
-p = b.crear(producto)
-precio_neto = p.facturar()
+
+precio_neto = b.crear(producto).facturar()
 print(precio_neto)
