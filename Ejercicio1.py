371e8b2cebf56cb3dd9911f9923259f65931e768
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-17 15:12:45 +0200

    clase Si

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -5,3 +5,9 @@ class Bloque:
     def agregarInstruction(self, instruccion):
         self.instrucciones.append(instruccion)
 
+class Si:
+    def __init__(self, condicion, entonces, si_no):
+        self.condicion = condicion
+        self.entonces = entonces
+        self.si_no = si_no
+        
